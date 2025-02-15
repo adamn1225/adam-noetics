1d6ba6dad1da085f83b42c6d49ca5bdf32dbe88e
adamn1225 <anoah1225@gmail.com>
2025-02-15 14:22:19 -0500

    going another route

--- pages/dashboard/content/index.tsx
+++ pages/dashboard/content/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import DashboardLayout from "../UserLayout";
+import { Link } from 'react-router-dom';
 
 const PageBuilder = () => {
     return (
         <DashboardLayout>
             <div className="w-full h-screen">
                 Sorry - code is still crap/in-development
+                {/* <Link to="http://builder.nextnoetics.com">Go to Page Builder</Link> */}
+
             </div>
         </DashboardLayout>
     );
